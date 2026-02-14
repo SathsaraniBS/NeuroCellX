5f6e27454e0ccf109066fab393c0e4b3fe606cf6
SathsaraniBS <sandunibbs@gmail.com>
2026-02-14 23:22:43 +0530

    Add deprecated modules for MD4, PBKDF2, and Windows support; introduce PyJWT 2.11.0 metadata
    - Created `passlib.utils.md4` to provide a deprecated interface for MD4, warning users to use `passlib.crypto.digest.lookup_hash("md4").const()` instead.
    - Added `passlib.utils.pbkdf2` with deprecation warnings, advising users to switch to `passlib.crypto` for key derivation functions.
    - Introduced `passlib.win32` module with deprecation notices, recommending the use of `passlib.hash.nthash` and `passlib.hash.lmhash`.
    - Added metadata files for PyJWT version 2.11.0, including INSTALLER, METADATA, RECORD, REQUESTED, WHEEL, and license files.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { ToastProvider } from './contexts/ToastContext';
-
+import React, { useState, useEffect } from 'react';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import AdminDashboard from './pages/AdminDashboard';
 import Layout from './components/Layout';
+
+const API_URL = "http://127.0.0.1:8000/records/";
 
 function App() {
     return (
